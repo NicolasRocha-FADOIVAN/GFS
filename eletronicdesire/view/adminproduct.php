e7e7efa44ed22fe41a94bbcdac6dc777f5d95a3b
NicolasRocha-FADOIVAN <nicolas.goncalves.rocha@escola.pr.gov.br>
2025-10-01 11:47:28 -0300

    Add files via upload

---
+++ eletronicdesire/view/adminproduct.php
@@ -0,0 +1,27 @@
+<?php
+
+#INCLUI CONFIG PARA PEGAR A VARIÁVEL DA CONEXÃO E USA-LA COMO PARAMETRO NA FUNÇÃO REGISTER
+
+include 'config.php';
+include 'system/adminSys.php';
+
+if(isset($_POST['manageProducts'])){
+    manageProduct($conn);
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="POST">
+        <input type="text" name="name">         <br>
+        <button type="submit" name="manageProducts">Criar Conta</button>
+    </form>
+</body>
+</html>

2a5841fded9a66c982d5158f77d73a83e868f864
NicolasRocha-FADOIVAN <nicolas.goncalves.rocha@escola.pr.gov.br>
2025-10-07 23:18:51 -0300

    Add files via upload

---
+++ eletronicdesire/view/cart.php
@@ -0,0 +1,25 @@
+<?php
+
+include 'config.php';
+include 'system/ShopSys.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <?php foreach(shoProduct($conn) as $list): ?>
+        <li>
+            <a href="index.php?route=product&idproduto=<?php echo $list[0]; ?>">
+            <?php print_r($list[1]); ?>
+        </li>
+        <?php endforeach ?>
+    </ul>
+</body>
+</html>
